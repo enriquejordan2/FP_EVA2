c31dceaadbc9705ec6abf1505b90edb349cf25c7
enriquejordan2 <L24550342@chihuahua2.tecnm.mx>
2024-08-26 16:51:41 -0600

    Add files via upload

---
+++ eva1_2_variables/EVA1_2_VARIABLES.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_variables;
+
+/**
+ *
+ * @author Jose Enrique
+ */
+public class EVA1_2_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){ 
+        // TODO code application logic here
+      System.out.println("HOLA MUNDO!!"); //Cadena de texto
+     System.out.println(100);// Un número entero
+     System.out.println('c'); //Un caracter
+     System.out.println(true); //valores logicos
+     System.out.println(false);
+     System.out.println(1000.52); //números con fracción (con punto flotante)
+     
+     //TIPOS DE DATOS
+     //ENTEROS
+     //int 
+     //long 
+     //byte
+     //short
+     //PUNTO FLOTANTE
+     //double 
+     //float
+     //TEXTO
+     //string
+     //CARACTER
+     //char
+     //LOGICOS (true false)
+     //boolean
+     
+     //creacion de una bariable
+     //DECLARACION
+     //tipo de dato, nombre de la variable
+     //que operaciones debe o se puede hacer
+     //cuanta memoria va a utilizar
+     //ASIGNACiÓN--> poner un valor en la variable
+     //creacion y uso de la variable 
+     
+     int edad; // declaro un a variable de tipo entero, llama
+               //edad
+     //double edad;//NO PODEMOS TENER DOS VARIABLES CON EL MISMO NOMBRE     
+      String nombre;
+      String apellido;
+      double promedio;
+      //nombre de la variable
+      edad = 18; //estoy asignado el valor 18 dentro de la 
+                 // variable edad
+      
+      System.out.println(edad);
+     
+    }
+}
+   
